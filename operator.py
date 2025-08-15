194ec2500980fe52266f531d471638feaa2b0a5b
sudikshashrestha <nabinas293@gmail.com>
2025-08-16 00:49:17 +0545

    operator practiced

---
+++ operator.py
@@ -0,0 +1,53 @@
+#print
+print("hello")
+print("hello my name is sudiksha", i am 13 year old)
+
+#variables
+name= "Sudiksha"
+age=13
+print("name")
+print(="age")
+
+#sum
+25+5
+7+3
+
+#data type
+print(type(name))
+print(typw(age))
+ 
+type of operaters
+
+#1 arithmetic operators
+a=5
+b=2
+sum=a+b
+print(sum)
+
+a=5
+b=2
+print(a + b)
+
+#2 relation operators
+a=50
+b=20
+print(a == b) #False
+print(a != b) #True
+print(a >= b) #False
+print(a <= b) #True
+
+#3 assigment operaters
+num=10
+num += 10
+print("num : ", num)
+
+#4 ligical operators
+#not
+print(not False)
+#and
+value 1= True
+value 2= True
+print("and operator :", value 1 & value 2)
+#or
+print("or operator :", value 1 & value 2)
+ 
