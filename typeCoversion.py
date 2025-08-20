f927abf5bf741dfefaa5be33682d4b35912646e4
sudikshashrestha <nabinas293@gmail.com>
2025-08-21 00:49:24 +0545

    type coversion

--- typeCoversion.py
+++ typeCoversion.py
@@ -3,8 +3,8 @@ a = 2
 b = 3.2  
 sum = a+b #2+3.2= 5.2
 print(sum)
-#type castiong 
-a = int("2") #we can use any vriable
+#type casting 
+a = int("2") #we can use any variable
 b = 3.2
 
 print (type(a))
